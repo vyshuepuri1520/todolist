50d5c6693aa2824519475e4712ef87e40c2140e3
saivyshu1520@gmail.com <saivyshu1520@gmail.com>
2023-09-21 22:47:54 +0530

    todolist completed

---
+++ src/components/TodosList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const TodosList = ({todos,setTodos}) =>{
+    return(
+        <div>
+            {todos.map((todo)=>(
+                <li className="list-item" key = {todo.id}>
+                    <input type = "text" value = {todo.title} className="list"
+                     onChange={(event)=> event.preventDefault()}/>
+                     <div>
+                        <button className = "button-complete task-button">
+                            <i className="fa fa-check-circle"></i>
+                        </button>
+                        <button className = "button-edit task-button">
+                            <i className="fa fa-edit"></i>
+                        </button>
+                        <button className = "button-delete task-button">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                     </div>
+                </li>
+            ))}
+        </div>
+    )    
+}
+export default TodosList;
